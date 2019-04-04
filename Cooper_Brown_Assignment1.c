9b5d0f09d55c8da9d114fea2cd88655d9da7579a
LegendX5437 <00coops@gmail.com>
2019-04-04 16:11:12 +1100

    Begun modification for command line io. testing initial concept

--- Cooper_Brown_Assignment1.c
+++ Cooper_Brown_Assignment1.c
@@ -1,26 +1,57 @@
 #include <stdio.h>
 #include <string.h>
 
-void main()
+char inbuiltCommandSelector();
+
+char inbuiltCommandSelector()
 {
-    const char * instruction1 = "commands";
+    const char * prntCommands = "commands",
+                 task1 = "encode -rotation -key",
+                 task2 = "decode -rotation -key",
+                 task3 = "encode -substitution -key",
+                 task4 = "decode -substitution -key",
+                 task5 = "decode -rotation",
+                 task6 = "decode -substitution";
     char command[100];
+
     printf("Please enter a command. (type commands for list of commands) \n > ");
-    scanf("%[^\n]s", command);
-    // printf("%s", command); // for testing if the input was entered correctly
-    if (strcmp(command,instruction1) == 0)
+    
+    do 
     {
-        printf("commands =>     gets list of commands \n");
-        printf("encode =>     gets list of commands \n");
-        printf("commands =>     gets list of commands \n");
+        scanf("%[^\n]s", command);
+
+        if (strcmp(command, task1) == 0)
+        {
+            printf("> %s          =>     prints list of commands \n", prntCommands);
+            printf("> %s          =>     encodes rotation cypher with key given \n", task1);
+            printf("> %s          =>     decodes rotation cypher with key given \n", task2);
+            printf("> %s          =>     encodes substitution cypher with key given \n", task3);
+            printf("> %s          =>     decodes substitution cypher with key given \n", task4);
+            printf("> %s          =>     decodes rotation cypher without key \n", task5);
+            printf("> %s          =>     decodes substitution cypher without key \n", task6);
+        } //else if (strcmp(command, task2))
+
+
+
+
+    } while (1 == 1);
+    
+    return 1;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        char task_number = inbuiltCommandSelector();
+    } else 
+    {
+        printf("Yahoo");
     }
 
-    /*
-	char text[100];
-	printf("Enter text to process: ");
-	scanf("%s", text); // alternatively &text[0]
-    */
+  
 
+   return 0;
 }
 
 
