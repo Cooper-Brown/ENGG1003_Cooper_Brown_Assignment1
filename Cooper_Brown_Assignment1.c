a90a94b93d8e1ed5174b53824e2419251df1fd1c
LegendX5437 <00coops@gmail.com>
2019-04-27 18:37:33 +1000

    program fully functional. Now adding comments

--- Cooper_Brown_Assignment1.c
+++ Cooper_Brown_Assignment1.c
@@ -27,76 +27,99 @@ char crossReference2(char inputText[2100][100], char wordBank[2100][100], char m
 // ___________________________________________________________ MAIN _________________________________________________________ //
 int main()
 {
-    char taskNumber = 0;
+    // _____Initialising variables to be used in MAIN_____
+    char taskNumber = 0; // holds a number that is used to choose which task will be performed
 
-    char rotationKey = 0;
-    char originalLetters[] = "abcdefghijklmnopqrstuvwxyz";
-    char substitutionKey[27];
+    char rotationKey = 0; // used to hold a number from 0-26 that will represent a caesar cypher key
+    char originalLetters[] = "abcdefghijklmnopqrstuvwxyz"; // an array holding the letters of the alphabet. 
+    char substitutionKey[27]; // an empty array that will contain an ordered list of letters that the letters in the above array will be substituted with
 
-    char successful = 0;
+    char successful = 0; // a boolean variable used to hold the return status of the tasks. 1 = successful and 0 = not successful
 
-    taskNumber = getTaskAndKey(&rotationKey, substitutionKey);
+    taskNumber = getTaskAndKey(&rotationKey, substitutionKey); // gets the tasknumber and the needed key from the function getTaskAndKey
 
-    switch (taskNumber)
+    switch (taskNumber) // switch case to choose between tasks based on tasknumber
     {
-        case 1:
-            printf("Attempting to complete task %d...\n", taskNumber);
-            successful = task1and2(rotationKey);
+        case 1: // This case will encrypt the text using a caesar cypher with given key
+            printf("Attempting to complete task %d...\n", taskNumber); // informs the user about what task is being performed
+            successful = task1and2(rotationKey); // attempts to perform the encryption. If the process fails, successful becomes 0
             break;
-        case 2:
+        case 2: // This case will decrypt the text using a caesar cypher with given key
             printf("Attempting to complete task %d...\n", taskNumber);
-            successful = task1and2(-1*rotationKey);
+            successful = task1and2(-1*rotationKey); // attempts to perform the encryption. Key is negative because letters need to be rotated the other way to get back to what they were before the original encrypion
             break;
-        case 3:
+        case 3: // this case will encrypt the text using a substitution key
             printf("Attempting to complete task %d...\n", taskNumber);
-            successful = task3and4(originalLetters, substitutionKey);
+            successful = task3and4(originalLetters, substitutionKey); // attempts to perform encryption by substituting the letters of originalLetters with letters of substitutionKey
             break;
-        case 4:
+        case 4: // this case will decrypt the text using a substitution key
             printf("Attempting to complete task %d...\n", taskNumber);
-            successful = task3and4(substitutionKey, originalLetters); // input parameters swapped so key is 'reversed'
+            successful = task3and4(substitutionKey, originalLetters); // same as case 3, except parameters swapped so key is 'reversed' for decryption
             break;
-        case 7:
-            taskNumber = 5;
+        case 7: // this case will initially attempt a rotation cypher, and if a satisfactory result is not achieved it will attempt to crack it as though it was substitution
+            taskNumber = 5; // initially attempts the rotation decryption
             printf("Attempting to complete task %d...\n", taskNumber);
-            successful = task5();
-            if (successful == 0)
+            successful = task5(); // task5 will perform the necessary rotations and spelling checks
+            if (successful == 0) // if this fails, substitution decryption is attempted
             {
-                printf("Task %d failed\n", taskNumber);
+                printf("Task %d failed\n", taskNumber); // informs the user that the caesar decryption has failed
                 taskNumber = 6;
-                printf("Attempting to complete task %d...\n", taskNumber);
-                successful = task6(originalLetters);
+                printf("Attempting to complete task %d...\n\n", taskNumber);
+                successful = task6(originalLetters); // task 6 will perform the decryption
             }
             break;
     }
     if (successful)
-        printf("Task %d has been completed.\n", taskNumber);
-        // print output file to screen
-    else
+    {
+        printf("Task %d has been completed.\nBelow is a copy of the contents of outputFile.txt\n\n", taskNumber);
+        FILE *outputFile;
+        outputFile = fopen("outputFile.txt", "r"); // opens the output file so the contents can be printed to the screen
+        // the file's existance does not need to be checked as in order to get to this state it must have existed in the first place
+        char fileCharacter;
+        while (fscanf(outputFile, "%c", &fileCharacter) == 1) // this loop prints every character in the file as is
+        {
+            printf("%c", fileCharacter);
+        }
+        fclose(outputFile);
+    }
+        
+    else // if the task was not successful, the user is informed of this
         printf("No task could be completed.\n");
 
     return 0;
 }
 
-
-
 // _______________________________________________________ FUNCTIONS _________________________________________________________ //
 
+/*  This function serves the purpose of determining the operation that the user wants to perform.
+    Input:
+    It takes in two pointers to the location of where the keys are stored in the calling function
+    
+    Return:
+    The number of the task to be performed. The location of one of the pointers is also changed to a valid keys. 
+    
+    Functionality:
+    1. Opens keyFile.txt
+    2. grabs the wanted operation type(encode/decode) from the first line of the file.
+    3. input matching through scanf() determines the type of key the user has entered.
+    4. The data from steps 3 and 4 are used to select the desired task
+*/
 char getTaskAndKey(char *rotationKey, char *substitutionKey)
 {
     FILE *keyFile;
-    char command[100];
+    char command[100]; // this array is used to hold the text typed in the first line of the program
     char taskNumber = 0;
-    fpos_t position;
+    fpos_t position; // this is an object used to indicate the read position of the file. It is used to re-read lines that have already been read
     
     keyFile = fopen("keyFile.txt", "r");
 
-    if (keyFile == NULL)
+    if (keyFile == NULL) // this statement checks if the file exists, to avoid crashes later on and give the user information about why the program is not working
     {
         printf("Error: keyFile.txt is not in the current directory\n");
         return 0;
     }
 
-    fscanf(keyFile, "%s", command);
+    fscanf(keyFile, "%s", command); // 
     fgetpos(keyFile, &position);
     if (strcmp(strlwr(command),"encode") == 0)
     {
@@ -213,7 +236,8 @@ char task3and4(char *originalLetters, char *substituteLetters)
         }
         fprintf(outputFile, "%c", inputCharacter);
     }
-
+    fclose(inputFile);
+    fclose(outputFile);
     return 1;
 }
 
@@ -283,7 +307,7 @@ char task5()
         //printf("Hits: %d noHits: %d\n", hit, noHit);
         if ((hit*1.0)/(hit+noHit) > 0.5)
         {
-            printf("The key is %d\n", key);
+            printf("The key used to originally ENCODE the text is %d\n\n", key);
             task1and2((-1)*key);
             return 1;
         }
@@ -327,7 +351,6 @@ char task6(char *originalLetters)
     if (errorFlag == 1) // having the flag may seem useless but if both files are missing I want an error message for each.
         return 0;
 
-    printf("Starting loops\n");
     for (int word = 0; word < maxWords; word++)
         {
             for (int letter = 0; letter < maxCharacters; letter++)
@@ -366,8 +389,10 @@ char task6(char *originalLetters)
         inputLetterFrequencies[highestIndex] = tempc;
     }
          
-    printf("Key: %s\n", inputLetterFrequencies);
-
+    printf("Below is the calculated key for this task\n");
+    for (int index = 0; index < 26; index++)
+        printf("%c --> %c\n", standardLetterFrequencies[index], inputLetterFrequencies[index]);
+    printf("This key was used to ENCODE the data to its encrypted state.\nThe key used by this program to decrypt the text has the arrow inverted\ni.e. a --> b becomes b --> a\n\n");
     /*
     for (int word = 0; word < maxWords; word++)
     {
@@ -383,16 +408,8 @@ char task6(char *originalLetters)
             }
         }
     }
+    */
 
-
-    for (int word = 0; word < maxWords; word++)
-    {
-        if (strcmp(inputText[word], "anh") == 1)
-            
-        
-    }
-    
-*/
     task3and4(inputLetterFrequencies, standardLetterFrequencies);
     return 1;
 }
